467153f91fb845affd36979df069d1e31383e9bc
heiwais25 <heiwais25@gmail.com>
2020-03-11 12:12:50 +0900

    Change level

--- src/hooks/useKakaoMap.ts
+++ src/hooks/useKakaoMap.ts
@@ -18,7 +18,6 @@ export default (
   useEffect(() => {
     const map = new window.kakao.maps.Map(ref.current, options);
     // API's maximum length is about 10000 which is zoom 6
-    map.setMaxLevel(6);
     setMap(map);
 
     // eslint-disable-next-line react-hooks/exhaustive-deps
